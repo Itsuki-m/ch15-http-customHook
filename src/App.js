5ebde7b8e60506edd43db531a735ea243532d49b
Itsuki-m <mizuharu_sawaka14@yahoo.co.jp>
2021-10-13 14:29:53 +0900

    App.jsにuse-httpカスタムフック実装 まだuseEffectの追記必要だが起動する #191using the custom Http hook

--- src/App.js
+++ src/App.js
@@ -5,38 +5,35 @@ import React, { useEffect, useState } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
+import useHttp from './hooks/use-http';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-hooks-sc15-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json'
-      );
+  // use-http.jsのapplyDataの部分
+  // custom hookプラスこのApp.js特有のdata specific部分を書いている
+  const transformTasks = (tasksObj) => {
+    const loadedTasks = [];
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
+    for (const taskKey in tasksObj) {
+      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
     }
-    setIsLoading(false);
+
+    setTasks(loadedTasks);
   };
+
+  // use-http.js(custom hook)利用する
+
+  const {
+    isLoading,
+    error,
+    sendRequest: fetchTasks,
+  } = useHttp(
+    {
+      url: 'https://react-hooks-sc15-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json',
+    },
+    transformTasks
+  );
 
   useEffect(() => {
     fetchTasks();
